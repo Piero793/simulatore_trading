c312f8b04f9ccada292d3dd81a57e872aedafd16
“Piero793” <pierolacitignola7@gmail.com>
2025-04-23 12:12:37 +0200

    Validation added

--- src/main/java/it/epicode/simulatore_trading/transazioni/TransazioneRequest.java
+++ src/main/java/it/epicode/simulatore_trading/transazioni/TransazioneRequest.java
@@ -12,12 +12,16 @@ public class TransazioneRequest {
     @NotBlank
     @Pattern(regexp = "Acquisto|Vendita", message = "Tipo transazione deve essere 'Acquisto' o 'Vendita'")
     private String tipoTransazione; //  "Acquisto" o "Vendita"
-    @Min(1)
+
+    @Min(value = 1, message = "La quantità deve essere almeno 1")
     private int quantita; //  Deve essere almeno 1
-    @Positive
+
+    @Positive(message = "Il prezzo unitario deve essere positivo")
     private double prezzoUnitario; //  Valore positivo obbligatorio
-    @NotNull
+
+    @NotNull(message = "L'ID dell'azione non può essere nullo")
     private Long azioneId; //  ID dell’azione collegata
-    @NotBlank
+
+    @NotBlank(message = "Il nome utente non può essere vuoto")
     private String nomeUtente;
 }
